85755888ffd2596e192e4ce8248d544fd85886cf
Gabriel Borges Diorato <70965857+borges-boss@users.noreply.github.com>
2023-04-20 17:27:36 -0300

    Add files via upload

---
+++ controllers/GetProductTypes.php
@@ -0,0 +1,12 @@
+<?php
+include('../src/logic/Database.php');
+include('../src/modules/product/object/Product.php');
+include('../src/modules/product/object/ProductType.php');
+include('../src/modules/product/object/ProductSpecific.php');
+include('../src/modules/product/repository/ProductRepository.php');
+include('../src/modules/product/service/ProductService.php');
+
+$productService = new ProductService();
+$response = $productService->fetchProductTypes();
+echo $response;
+?>
